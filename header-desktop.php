da93aff687a9c4a8ef58495c82f21f17442a02c1
tekleang <tekleangtomnerb@gmail.com>
2018-12-06 11:37:45 +0700

    1

---
+++ header-desktop.php
@@ -0,0 +1,77 @@
+<?php 
+	include_once('_config_inc.php');
+	$BATH_URL=BASE_URL;
+ ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>LOCA RESIDENT</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="<?php echo $BATH_URL;?>assets/css/animate.min.css">
+	<link rel="stylesheet" href="<?php echo $BATH_URL;?>assets/css/desk-style.css">
+	
+	<script src="<?php echo $BATH_URL;?>assets/scripts/jquery.js"></script>
+	<script src="<?php echo $BATH_URL;?>assets/scripts/main.js"></script>
+	<link rel="stylesheet" type="text/css" href="<?php echo $BATH_URL;?>assets/css/slide-animate.css">
+	<link rel="stylesheet" type="text/css" href="<?php echo $BATH_URL;?>assets/css/slick.css">
+	<script src="<?php echo $BATH_URL;?>assets/scripts/slick.min.js"></script>
+	<script src="<?php echo $BATH_URL;?>assets/scripts/slide-animate.js"></script>
+	<script src="<?php echo $BATH_URL;?>assets/scripts/function clickshow.js"></script>
+	<script src="<?php echo $BATH_URL;?>assets/scripts/wow.min.js"></script>
+	<script>new WOW().init();</script>
+	<link rel="stylesheet" href="<?php echo $BATH_URL;?>assets/css/responsive.css">
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+</head>
+<body>
+	<div id="wrapper">
+		<div id="header">
+			<div class="main-box-nav">
+		        <!-- desktop menu -->
+		        <div class="container-fluid container-fluid-menu">
+		          <div class="container" style="width: 87.5%;">
+		            <nav class="nav-bottom">
+
+		              <div class="box-tool-top">
+		                <div class="hamburger" id="hamburger-menu">
+		                  <div class="bar1"></div>
+		                    <div class="bar2"></div>
+		                    <div class="bar3"></div>
+		                </div>
+		              </div>
+		              
+		              <div class="sub-nav-bottom">
+		                <ul class="menu">
+		                  	<li><a href="<?php echo $BATH_URL;?>desktop/content-team.php">team</a></li>
+							<li><a href="<?php echo $BATH_URL;?>desktop/content-contact.php">contact</a></li>
+							<li class="fix"><a href="<?php echo $BATH_URL;?>desktop/content-register.php"">register now</a></li>
+		                </ul>
+		              </div>
+
+		              <div class="slidbar">
+		              	<div class="container">
+		              		 <div class="box">
+		              		 	<ul class="menu">
+				                  	<li class="wow fadeInLeft slow"><a href="<?php echo $BATH_URL;?>desktop/content-location.php" title="">THE LOCATION</a></li>
+									<li><a href="<?php echo $BATH_URL;?>desktop/content-plans.php" title="">FLOOR PLANS</a></li>
+									<li><a href="<?php echo $BATH_URL;?>desktop/content-design.php" title="">INTERIORS DESIGN</a></li>
+									<li><a href="<?php echo $BATH_URL;?>desktop/content-amenities.php" title="">OUR AMENITIES</a></li>
+									<li><a href="<?php echo $BATH_URL;?>desktop/content-developer.php" title="">DEVELOPER PARTNER</a></li>
+									<li><a href="<?php echo $BATH_URL;?>desktop/content-events.php" title="">NEWS & EVENTS</a></li>
+									<li><a href="<?php echo $BATH_URL;?>desktop/content-galleries.php" title="">OUR GALLERIES</a></li>
+									<li><a href="<?php echo $BATH_URL;?>desktop/content-team.php" title="">OUR TEAM</a></li>
+									<li><a href="<?php echo $BATH_URL;?>desktop/content-contact.php" title="">CONTACT US</a></li>
+				                </ul>
+		              		 </div>
+		              	</div>
+		               
+		              </div>
+		              
+		            </nav>
+		          </div>
+		        </div>
+		    </div>
+		</div>
+	</div>
